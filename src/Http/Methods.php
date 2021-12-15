75740f396f3accdec31ea4dc107bc81ccb952816
Salam <salamwaddah@gmail.com>
2021-12-15 22:45:22 +0400

    add more http methods

--- src/Http/Methods.php
+++ src/Http/Methods.php
@@ -5,8 +5,12 @@ namespace SalamWaddah\Constants\Http;
 class Methods
 {
     public const GET = 'GET';
+    public const HEAD = 'HEAD';
     public const POST = 'POST';
     public const PUT = 'PUT';
     public const DELETE = 'DELETE';
+    public const CONNECT = 'CONNECT';
+    public const OPTIONS = 'OPTIONS';
+    public const TRACE = 'TRACE';
     public const PATCH = 'PATCH';
 }
