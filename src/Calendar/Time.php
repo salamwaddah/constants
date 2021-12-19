5f565bc83cbb549a97b10553d28b4b3706ddee38
Salam <salamwaddah@gmail.com>
2021-12-19 13:59:26 +0400

    add more time constants for cache

--- src/Calendar/Time.php
+++ src/Calendar/Time.php
@@ -8,6 +8,8 @@ class Time
     public const ONE_SECOND = 1;
     public const MINUTE_IN_SECONDS = 60;
     public const HOUR_IN_SECONDS = 3600;
+    public const TEN_HOURS_IN_SECONDS = 36000;
+    public const HALF_DAY_IN_SECONDS = 43200;
     public const DAY_IN_SECONDS = 86400;
     public const WEEK_IN_SECONDS = 604800;
     public const MONTH_IN_SECONDS = 2592000;
@@ -16,6 +18,8 @@ class Time
     // minute based
     public const ONE_MINUTE = 1;
     public const HOUR_IN_MINUTES = 60;
+    public const TEN_HOUR_IN_MINUTES = 600;
+    public const HALF_DAY_IN_MINUTES = 720;
     public const DAY_IN_MINUTES = 1440;
     public const WEEK_IN_MINUTES = 10080;
     public const MONTH_IN_MINUTES = 43200;
