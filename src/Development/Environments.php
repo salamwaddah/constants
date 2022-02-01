47d116b34396f85a8eb80241465e4ed9ecc2cfba
Salam <salamwaddah@gmail.com>
2022-02-01 14:11:58 +0400

    close #1 add sandbox to environment

--- src/Development/Environments.php
+++ src/Development/Environments.php
@@ -11,6 +11,7 @@ class Environments
     public const INTERNAL = 'internal';
     public const LOCAL = 'local';
     public const PRODUCTION = 'production';
+    public const SANDBOX = 'sandbox';
     public const STAGE = 'stage';
     public const STAGING = 'staging';
 }
